d17e73e0cec1aa2fefdf1a153bbf63b7b202345c
Oriol Boix Anfosso <orboan@pluralcamp.com>
2023-05-01 17:51:08 +0000

    exercise completed

---
+++ src/main/java/com/pluralcamp/servlets/ViewMenuServlet.java
@@ -0,0 +1,34 @@
+package com.pluralcamp.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import com.pluralcamp.data.MenuDataService;
+import com.pluralcamp.domain.MenuItem;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ViewMenuServlet extends HttpServlet {
+
+    public void service(HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+                PrintWriter out = response.getWriter();
+                response.setContentType("text/html");
+
+                MenuDataService menuDataService = new MenuDataService();                
+                List<MenuItem> menuItems = menuDataService.getFullMenu();
+
+                out.println("<html><body><h1>Quark's Restaurant</h1>");
+                out.println("<h2>Menu</h2><ul>");
+
+                for(MenuItem menuItem : menuItems){
+                    out.printf("<li>%s</li>%n", menuItem);
+                }
+
+                out.println("</ul></body></html>");
+                out.close();
+    }
+}
